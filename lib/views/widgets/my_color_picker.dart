55fec75183b6fad0a5d51e10ae25c90bb26b6d80
Amr Tamer <Amrtamer23mj@gmail.com>
2023-03-08 20:04:11 +0200

    color picker for add quotes

---
+++ lib/views/widgets/my_color_picker.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+
+Color? showColorPickerDialog(BuildContext context) {
+  Color selectedColor = Colors.white; // default color
+
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Pick a color'),
+        content: SingleChildScrollView(
+          child: ColorPicker(
+            pickerColor: selectedColor,
+            onColorChanged: (color) => selectedColor = color,
+            pickerAreaHeightPercent: 0.8,
+          ),
+        ),
+        actions: [
+          ElevatedButton(
+            child: const Text('CANCEL'),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          ElevatedButton(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+  return selectedColor;
+}
